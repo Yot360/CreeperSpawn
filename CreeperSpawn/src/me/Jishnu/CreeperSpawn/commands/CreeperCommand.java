1a7a5d4faca736caac73d67106175e8f4364ae81
Yot360 <65982248+Yot360@users.noreply.github.com>
2021-11-25 22:01:46 +0100

    Update CreeperCommand.java

--- CreeperSpawn/src/me/Jishnu/CreeperSpawn/commands/CreeperCommand.java
+++ CreeperSpawn/src/me/Jishnu/CreeperSpawn/commands/CreeperCommand.java
@@ -12,15 +12,22 @@ import me.Jishnu.CreeperSpawn.Main;
 
 public class CreeperCommand implements CommandExecutor {
 	@Override
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		Player player = (Player) sender;
-		Location loc = player.getLocation();
-		World w = player.getWorld();
-		for(int i = 0; i < 10; i++) {
-			w.spawnEntity(loc, EntityType.CREEPER);
-		}
-
-		return false;
-	}
-     
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player){
+            Player player = (Player) sender;
+            if (args.length == 0){
+                sender.sendMessage(ChatColor.RED+"Please choose a number creepers to spawn");
+            }else if (args.length == 1){
+                int nmb = Integer.parseInt(args[0]);
+                Location loc = player.getLocation();
+                World w = player.getWorld();
+                for(int i = 0; i < nmb; i++) {
+                    w.spawnEntity(loc, EntityType.CREEPER);
+                }
+            }else {
+                player.sendMessage(ChatColor.RED+"Please only enter one argument");
+            }
+        }
+        return false;
+    } 
 }
